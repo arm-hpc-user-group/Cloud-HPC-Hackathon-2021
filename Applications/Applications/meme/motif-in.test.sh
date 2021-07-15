3539c75ef69c9bc1eb7392dd74f01290a2abae52
Ricardo Jesus <rj.bcjesus@gmail.com>
2021-07-15 13:33:41 +0000

    Add more tests

--- Applications/Applications/meme/motif-in.test.sh
+++ Applications/Applications/meme/motif-in.test.sh
@@ -12,7 +12,20 @@ T1)
 	[ $(ls -1 results/motif-in.streme.klf1.text/*.eps | wc -l) -ge 1 ] && echo PASS || echo FAIL
 	[ $(ls -1 results/motif-in.streme.klf1.xml/*.eps  | wc -l) -ge 1 ] && echo PASS || echo FAIL
 	[ $(ls -1 results/motif-in.streme.klf1.html/*.eps | wc -l) -ge 1 ] && echo PASS || echo FAIL
+
 	;;
+
+T2)
+	$meme2images -eps results/motif.streme.klf1.extdna/streme.txt  results/motif-in.streme.klf1.extdna.text
+	$meme2images -eps results/motif.streme.klf1.extdna/streme.xml  results/motif-in.streme.klf1.extdna.xml
+	$meme2images -eps results/motif.streme.klf1.extdna/streme.html results/motif-in.streme.klf1.extdna.html
+
+	[ $(ls -1 results/motif-in.streme.klf1.extdna.text/*.eps | wc -l) -ge 1 ] && echo PASS || echo FAIL
+	[ $(ls -1 results/motif-in.streme.klf1.extdna.xml/*.eps  | wc -l) -ge 1 ] && echo PASS || echo FAIL
+	[ $(ls -1 results/motif-in.streme.klf1.extdna.html/*.eps | wc -l) -ge 1 ] && echo PASS || echo FAIL
+
+	;;
+
 *)
 	echo FAIL
 	;;
